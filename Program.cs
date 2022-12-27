1284da109f08d3d69572979cb2081dcf357937cc
Jackson Duarte <jacksonwillerduarte@gmail.com>
2022-12-26 21:34:09 -0300

    config startup class

--- Program.cs
+++ Program.cs
@@ -1,25 +1,11 @@
+using EmpresaApp;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
+var startup =  new Startup(builder.Configuration);
+startup.ConfigurationService(builder.Services);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
-
+startup.Configure(app, app.Environment);
 app.Run();
